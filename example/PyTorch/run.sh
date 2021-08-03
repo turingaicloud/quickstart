f351235c0514ea7ae1b58b7ad553bac60d050114
774343428 <sun.dec@husky.neu.edu>
2021-08-03 20:21:43 +0800

    Adaptation to the new environment & add argument

--- example/PyTorch/run.sh
+++ example/PyTorch/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /mnt/sharefs/home/user6/.Miniconda3/etc/profile.d/conda.sh
-conda activate torch-env
+source /mnt/home/coco/.Miniconda3/etc/profile.d/conda.sh
+conda activate 755c9df7b9e3837d5eb7d80ee6bab896
 
-VISIBLE_CUDA_DEVICES='0' GLOO_SOCKET_IFNAME=enp1s0f1 python ${TACC_WORKDIR}/mnist.py --epoch=3 \
+VISIBLE_CUDA_DEVICES='0' GLOO_SOCKET_IFNAME=eno1 python ${TACC_WORKDIR}/mnist.py --epoch=3 \
