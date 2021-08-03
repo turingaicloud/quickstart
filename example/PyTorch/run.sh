c9a7ae3a4fe31809e76a19e3dc8bb8a03dc8d88b
774343428 <sun.dec@husky.neu.edu>
2021-08-03 20:30:11 +0800

    Delete auto files

--- example/PyTorch/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /mnt/home/coco/.Miniconda3/etc/profile.d/conda.sh
-conda activate 755c9df7b9e3837d5eb7d80ee6bab896
-
-VISIBLE_CUDA_DEVICES='0' GLOO_SOCKET_IFNAME=eno1 python ${TACC_WORKDIR}/mnist.py --epoch=3 \
