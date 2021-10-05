c7b49336d02cf100e01c9a7b2e09b5991c5a4736
774343428 <sun.dec@husky.neu.edu>
2021-10-05 19:27:12 +0800

    update gpu, ddp, iplist

--- example/PyTorch/mnist.py
+++ example/PyTorch/mnist.py
@@ -35,10 +35,10 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, local_rank, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        data, target = data.to(local_rank), target.to(local_rank)
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
@@ -49,14 +49,14 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
-def test(args, model, device, test_loader, world_size):
+def test(args, model, local_rank, test_loader, world_size):
     model.eval()
     test_loss = 0
     correct = 0
     length = len(test_loader.dataset)/world_size
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+            data, target = data.to(local_rank), target.to(local_rank)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
@@ -67,6 +67,18 @@ def test(args, model, device, test_loader, world_size):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, length,
         100. * correct / length))
+
+def get_ip(str):
+    output = ""
+    if ',' in str:
+        output = str.split(",")[0]
+    elif '[' in str:
+        str1 = str.split("[")
+        str2 = str1[1].strip("]").split("-")
+        output = str1[0] + str2[0]
+    else:
+        print("!!! unknow ip format !!!")
+    return output
 
 def main():
     print("This is a PyTorch Example!!!")
@@ -96,27 +108,19 @@ def main():
                         help='Please add your dataset directory')
 
     args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
-    torch.manual_seed(args.seed)
+    # use_cuda = not args.no_cuda and torch.cuda.is_available()
+    # torch.manual_seed(args.seed)
+    # device = torch.device("cuda" if use_cuda else "cpu")
+    # kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
-    device = torch.device("cuda" if use_cuda else "cpu")
-
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     rank = int(os.environ['SLURM_PROCID'])
-    # rank = 0
     local_rank = int(os.environ['SLURM_LOCALID'])
-    # local_rank = 0
     world_size = int(os.environ['SLURM_NTASKS'])
-    # world_size = 1
-    def get_ip(iplist):
-        ip = iplist.split(',')[0]
-        return ip
     iplist = os.environ['SLURM_JOB_NODELIST']
-    ip = get_ip(iplist)
-    print(iplist)
-    # ip="gpu12"
-    print(ip, rank, local_rank, world_size)
+    ip = get_ip(os.environ['SLURM_STEP_NODELIST'])
+    print(rank, local_rank, world_size, iplist, ip)
+
     dist_init(ip, rank, local_rank, world_size)
     train_dataset = datasets.MNIST(args.datasetDir, train=True, download=False,
                        transform=transforms.Compose([
@@ -127,8 +131,8 @@ def main():
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.batch_size,
-        sampler=train_sampler,
-        **kwargs)
+        sampler=train_sampler
+        )
     test_dataset = datasets.MNIST(args.datasetDir, train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
@@ -138,16 +142,16 @@ def main():
     test_loader = torch.utils.data.DataLoader(
         test_dataset,
         batch_size=args.test_batch_size,
-        sampler=test_sampler,
-        **kwargs)
+        sampler=test_sampler
+        )
 
-    model = Net().to(device)
+    model = Net().to(local_rank)
     model = DistributedDataParallel(model)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader, world_size)
+        train(args, model, local_rank, train_loader, optimizer, epoch)
+        test(args, model, local_rank, test_loader, world_size)
 
     if (args.save_model):
         torch.save(model.state_dict(),"mnist_cnn.pt")
